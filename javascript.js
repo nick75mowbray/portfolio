4cf43989b689bd7fdeba4c06fd3cb8438734cfe2
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-12-01 20:01:44 +1030

    add vectary image, animate navbar and footer

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// script to show navbar on scroll up
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav-banner").style.top = "0";
+    document.getElementById("footer").style.bottom = "0";
+  } else {
+    document.getElementById("nav-banner").style.top = "-70px";
+    document.getElementById("footer").style.bottom = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+// fade in text on document load
+
+$( document ).ready(function(){
+    $('.fade-in').animate({opacity: '100'}, "slow");
+});
