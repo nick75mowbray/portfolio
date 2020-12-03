2d417d5c995eab7c653b41d25b48827c6dd24bbc
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-12-03 17:03:31 +1030

    attempt to add vertical scroll snapping

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ var currentScrollPos = window.pageYOffset;
     document.getElementById("nav-banner").style.top = "0";
     document.getElementById("footer").style.bottom = "0";
   } else {
-    document.getElementById("nav-banner").style.top = "-90px";
+    document.getElementById("nav-banner").style.top = "-120px";
     document.getElementById("footer").style.bottom = "-90px";
   }
   prevScrollpos = currentScrollPos;
@@ -14,8 +14,12 @@ var currentScrollPos = window.pageYOffset;
 
 // fade in text on document load
 
+
 $( document ).ready(function(){
+
     $('.fade-in').animate({opacity: '100'}, "slow");
+
 });
+
 
 
