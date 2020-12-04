f35d0a785303a543fbcc5b29321053b2f713bf8f
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-12-04 12:00:19 +1030

    add snap-scroll to sections

--- javascript.js
+++ javascript.js
@@ -1,20 +1,20 @@
-// script to show navbar on scroll up
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+// script to show navbar and footer on scroll up
+
+document.onwheel = function(event){
+
+  if (event.deltaY < 0) {
+    // show
     document.getElementById("nav-banner").style.top = "0";
     document.getElementById("footer").style.bottom = "0";
-  } else {
+  }
+  else {
+    // hide
     document.getElementById("nav-banner").style.top = "-120px";
     document.getElementById("footer").style.bottom = "-90px";
   }
-  prevScrollpos = currentScrollPos;
 };
 
 // fade in text on document load
-
-
 $( document ).ready(function(){
 
     $('.fade-in').animate({opacity: '100'}, "slow");
