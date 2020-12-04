9f3ab72ded44aa6a9ae29770c2e8ec849541a403
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-12-04 19:58:51 +1030

    styling changes to nav for smaller screens

--- javascript.js
+++ javascript.js
@@ -2,16 +2,27 @@
 
 document.onwheel = function(event){
 
-  if (event.deltaY < 0) {
+  var x = window.matchMedia("(max-width: 800px)")
+
+  if (x.matches) { // If media query less than or equal to 800
+    
     // show
     document.getElementById("nav-banner").style.top = "0";
     document.getElementById("footer").style.bottom = "0";
-  }
-  else {
+  } else {
+    // show hide nav bar on screens bigger than 800px
+    if (event.deltaY < 0) {
+    // show
+    document.getElementById("nav-banner").style.top = "0";
+    document.getElementById("footer").style.bottom = "0";
+    }
+    else {
     // hide
     document.getElementById("nav-banner").style.top = "-120px";
     document.getElementById("footer").style.bottom = "-90px";
+    }
   }
+
 };
 
 $( document ).ready(function(){
@@ -22,13 +33,6 @@ $( document ).ready(function(){
 });
 
 
-  // show and hide nav on mobile
-  $( document ).on("swipe",function(){
-    alert("swipedown");
-  });
-  $('.jumbotron').on('swipe',function(){
-    alert("swipeup..");
-  });
 
 
 
